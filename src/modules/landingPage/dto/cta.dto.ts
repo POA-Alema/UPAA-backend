26ba925164ab7432035064cd805ab78906237e03
Guilherme Chaves <guilherme.043@edu.pucrs.br>
2026-04-12 04:44:50 -0300

    feat(landing-page): create dto classes for the data structure and validation

---
+++ src/modules/landingPage/dto/cta.dto.ts
@@ -0,0 +1,17 @@
+import { ApiProperty } from '@nestjs/swagger';
+import { IsOptional, IsString } from 'class-validator';
+
+export class CtaDto {
+  @ApiProperty()
+  @IsString()
+  label: string;
+
+  @ApiProperty()
+  @IsString()
+  target: string;
+
+  @ApiProperty()
+  @IsString()
+  @IsOptional()
+  icon?: string;
+}
