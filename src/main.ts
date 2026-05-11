5c957e2d24bbe83f78ca15a72527ae72ba61c89a
Arthur Faleiro Barbosa <arthur.faleiro@edu.pucrs.br>
2026-05-11 13:40:13 -0300

    feat(main): add global validation pipe

--- src/main.ts
+++ src/main.ts
@@ -1,11 +1,19 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { SwaggerModule, DocumentBuilder } from '@nestjs/swagger';
+import { ValidationPipe } from '@nestjs/common';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
 
-  // Configuração do Swagger
+  app.useGlobalPipes(
+    new ValidationPipe({
+      whitelist: true,
+      forbidNonWhitelisted: true,
+      transform: true,
+    }),
+  );
+
   const config = new DocumentBuilder()
     .setTitle('Uma Porto Alegre Alemã API')
     .setDescription('API para gerenciamento de obras e pontos turísticos')
