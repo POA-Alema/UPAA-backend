3725b1090611cc5771c53cd0b46574557086b7c2
EduardoBarcellosz <eduardo.barcellos001@edu.pucrs.br>
2026-05-22 22:04:13 -0300

    chore: update default server port

--- src/main.ts
+++ src/main.ts
@@ -4,7 +4,8 @@ import { SwaggerModule, DocumentBuilder } from '@nestjs/swagger';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  const port = Number(process.env.PORT ?? 3001);
+
+  const port = Number(process.env.PORT ?? 3000);
 
   const config = new DocumentBuilder()
     .setTitle('Uma Porto Alegre Alemã API')
@@ -12,11 +13,14 @@ async function bootstrap() {
     .setVersion('1.0')
     .addTag('buildings')
     .build();
+
   const document = SwaggerModule.createDocument(app, config);
+
   SwaggerModule.setup('api', app, document);
 
   app.enableCors();
 
-  await app.listen(port);
+  await app.listen(port, '0.0.0.0');
 }
+
 bootstrap();
