89a72aa1a67b7d73d7eb395506b463b9334e1376
jpsalles21 <joaopedrosalles.dev@gmail.com>
2026-06-26 10:43:35 -0300

    feat: arquiteto crud

--- src/modules/architects/architects.module.ts
+++ src/modules/architects/architects.module.ts
@@ -2,9 +2,12 @@ import { Module } from '@nestjs/common';
 import { ArchitectsController } from './controllers/architects.controller';
 import { ArchitectsService } from './services/architects.service';
 import { PrismaService } from '../../prisma/prisma.service';
+import { S3Service } from '../../Utils/S3.service';
+import { AuthModule } from '../auth/auth.module';
 
 @Module({
+  imports: [AuthModule],
   controllers: [ArchitectsController],
-  providers: [ArchitectsService, PrismaService],
+  providers: [ArchitectsService, PrismaService, S3Service],
 })
 export class ArchitectsModule {}
